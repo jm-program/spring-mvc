aa3ad10de88650c543f19b74483d300fba4dd125
nickliq <nickliq@yandex.ru>
2021-09-30 16:52:18 +0400

    crud init commit

---
+++ src/main/java/web/util/Util.java
@@ -0,0 +1,68 @@
+package web.util;
+
+import com.mysql.jdbc.Driver;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class Util {
+
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mysql?useSSL=false");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "nick8nick9");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+    public Statement getStatement() throws SQLException {
+
+        final String user = "root";
+        final String password = "nick8nick9";
+        final String url = "jdbc:mysql://localhost:3306/mysql";
+
+        final Connection connection = DriverManager.getConnection(url, user, password);
+
+        return connection.createStatement();
+
+    }
+
+    public void statementClose(Statement statement) throws SQLException {
+        statement.close();
+    }
+
+}
